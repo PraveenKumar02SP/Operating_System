aa2aa0f7ae9e68385f46414e925910f9a3bd354f
PraveenKumar02SP <128678168+PraveenKumar02SP@users.noreply.github.com>
2023-11-03 09:15:48 +0530

    Delete SYSTEM CALLS.cpp

--- SYSTEM CALLS.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<unistd.h>
-int main()
-{
-	printf("PRAVEEN KUMAR S\n");
-  printf("Process ID: %d\n", getpid() );
-  printf("Parent Process ID: %d\n", getpid() );
-  return 0;
-}
